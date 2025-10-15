34ead16d3d8540768820786bdf932ce393eaf28b
GUILHERME REINERT <guilherme_reinert@estudante.sc.senai.br>
2025-10-15 20:25:48 -0300

    Entendendo LoginController e web

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -1,18 +1,31 @@
 <?php
 require_once __DIR__ . '/../models/User.php';
+// Inicia uma sessão PHP. Necessário para usar $_SESSION. Armazena o id do usuário
 session_start();
 
 class LoginController {
     public function login() {
+
+        // Verifica se a requisição HTTP foi feita usando o método POST.
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            // Pega o valor jogado no input type = email e armazena na variavel $email
             $email = $_POST['email'];
             $senha = $_POST['senha'];
 
+            // Cria uma instância da classe User
             $userModel = new User();
+
+            // Acessa o metodo login na classe User para fazer as validações
             $user = $userModel->login($email, $senha);
 
+            // Se for true
             if ($user) {
+
+                // Cria uma variavel $_SESSION que recebe o id do usuario
                 $_SESSION['usuario_id'] = $user['id'];
+
+                // Envia um cabeçalho HTTP que redireciona o navegador para tela-inicio.php
                 header('Location: ../public/tela-inicio.php');
                 exit;
             } else {
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@ require_once __DIR__ . '/../app/controllers/LoginController.php';
 
 $controller = new LoginController();
 
+// Verifica se contem o route no URL no form, isset retorna true
 if (isset($_GET['route'])) {
     switch ($_GET['route']) {
         case 'login':
